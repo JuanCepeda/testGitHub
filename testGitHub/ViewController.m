21cb3a74f0749700c48abe1b8019dbd8f94262ae
JuanCepeda <juan.emmanuel.cepeda@gmail.com>
2016-04-06 11:28:48 -0400

    Added Button to the second VC

--- testGitHub/ViewController.m
+++ testGitHub/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIButton *btnPushSecondViewController;
 
 @end
 
@@ -17,6 +18,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
